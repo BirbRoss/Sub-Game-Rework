f62dd33c690079077fb982b7d3a135c4530d2c19
BirbRoss <55706678+BirbRoss@users.noreply.github.com>
2019-11-17 09:21:18 +0000

    Version 0.0.07
    Added PT boat

---
+++ Submarine game revamp/Assets/Scripts/smoothLookAtTarget2D.cs
@@ -0,0 +1,27 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoothLookAtTarget2D : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5.0f;
+    public float adjustmentAngle = 0.0f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (target != null)
+        {
+            Vector3 difference = target.position - transform.position;
+            float rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            Quaternion newRot = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotz + adjustmentAngle));
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRot, Time.deltaTime * smoothing);
+        }
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
