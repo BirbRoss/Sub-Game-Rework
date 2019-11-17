058379176f4281f01282e3bfdd496dd11e92a9b6
BirbRoss <55706678+BirbRoss@users.noreply.github.com>
2019-11-17 11:15:02 +0000

    0.0.08b
    Still broken

---
+++ Submarine game revamp/Assets/Scripts/PT/PTMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PTMovement : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);
+        }
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
