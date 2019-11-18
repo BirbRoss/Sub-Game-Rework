48cd0c972f57870a1adffe4c5cbe84f462ab9fcf
BirbRoss <55706678+BirbRoss@users.noreply.github.com>
2019-11-18 03:10:36 +0000

    Version 0.0.11
    Added pathfinding (Doesn't work as intended)

---
+++ Submarine game revamp/Assets/Scripts/EnemyPathFinder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPathFinder : MonoBehaviour
+{
+    public Transform target;
+    private IAstarAI ai;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ai = GetComponent<IAstarAI>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(target != null && ai != null)
+        {
+            ai.destination = target.position;
+            ai.SearchPath();
+        }
+        Debug.DrawLine(transform.position, target.position, Color.green);
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
--- Submarine game revamp/Assets/Scripts/PTDamage.cs
+++ Submarine game revamp/Assets/Scripts/PTDamage.cs
@@ -6,12 +6,20 @@ using UnityEngine.UI;
 public class PTDamage : MonoBehaviour
 {
     public int damage;
+    public float resetTime = 0.25f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             collision.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+            GetComponent<Collider2D>().enabled = false;
+            Invoke("ResetTrigger", resetTime);
         }
+    }
+
+    private void ResetTrigger()
+    {
+        GetComponent<Collider2D>().enabled = true;
     }
 }
